8791490162ff5716d436785436349131330e74f8
Vlad <alpatof74@gmail.com>
2024-01-18 15:34:15 +0300

    added main

--- source/main.cpp
+++ source/main.cpp
@@ -1,3 +1,25 @@
 //
 // Created by Vlad on 18.01.2024.
 //
+#include <Windows.h>
+#include <thread>
+
+void HackThread(HMODULE hModule)
+{
+    while (!GetAsyncKeyState(VK_END))
+    {
+        Sleep(10);
+    }
+
+    FreeLibrary(hModule);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    {
+        DisableThreadLibraryCalls(hModule);
+        std::thread(HackThread, hModule).detach();
+    }
+    return TRUE;
+}
